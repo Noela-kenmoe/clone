64e3f0da2948ec28f5ce9ee92c8193725beb75d9
Noela-kenmoe <kenmoenoela@gmail.com>
2026-09-17 19:11:46 +0200

    modifier la ligne du meme dépot
    première modification de la ligne d'un meme fichier dans deux dépot différents
    Cette meme ligne sera modifiée dans le second dépot

---
+++ fon.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ int echange(int *a, int *b);
+
+int main(){
+    
+    int y, c ,d;
+    std::cout<<"entrer deux premiers nombres :";
+    std::cin>> c >> d;
+    std::cout<<"avant l'appel:"<< c << d;
+    y =echange(&c,&d);
+     std::cout<<"apres l'appel :"<< c << y ;
+     return 0;
+     
+}
+int echange (int *a, int *b)
+     { 
+      
+        int x;
+        *a = x;
+        *a = *b;
+        *b = x;
+        return 0;
+     }
